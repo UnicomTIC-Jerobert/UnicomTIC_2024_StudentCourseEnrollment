1bdc055d80924fdcf64a3e86f603e87d95b64f07
Unicom Training and Innovation Center <unico@UNICOMTIC20.unicomtic.com>
2024-08-07 16:30:24 +0530

    done desktop app

---
+++ StudentCourse.WinForm/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace StudentCourse.WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox CourseIDTextBox;
+        private System.Windows.Forms.TextBox CourseNameTextBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ViewButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.DataGridView CoursesGridView;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form Designer generated code
+
+     
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CourseIDTextBox = new System.Windows.Forms.TextBox();
+            this.CourseNameTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ViewButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.CoursesGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CoursesGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CourseIDTextBox
+            // 
+            this.CourseIDTextBox.Location = new System.Drawing.Point(84, 12);
+            this.CourseIDTextBox.Name = "CourseIDTextBox";
+            this.CourseIDTextBox.Size = new System.Drawing.Size(200, 20);
+            this.CourseIDTextBox.TabIndex = 0;
+            // 
+            // CourseNameTextBox
+            // 
+            this.CourseNameTextBox.Location = new System.Drawing.Point(84, 47);
+            this.CourseNameTextBox.Name = "CourseNameTextBox";
+            this.CourseNameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.CourseNameTextBox.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(20, 90);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ViewButton
+            // 
+            this.ViewButton.Location = new System.Drawing.Point(20, 130);
+            this.ViewButton.Name = "ViewButton";
+            this.ViewButton.Size = new System.Drawing.Size(75, 23);
+            this.ViewButton.TabIndex = 3;
+            this.ViewButton.Text = "View";
+            this.ViewButton.UseVisualStyleBackColor = true;
+            this.ViewButton.Click += new System.EventHandler(this.ViewButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(182, 90);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 4;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(101, 90);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 5;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // CoursesGridView
+            // 
+            this.CoursesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CoursesGridView.Location = new System.Drawing.Point(20, 170);
+            this.CoursesGridView.Name = "CoursesGridView";
+            this.CoursesGridView.Size = new System.Drawing.Size(300, 150);
+            this.CoursesGridView.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Course ID : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Course Name : ";
+       
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 350);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CourseIDTextBox);
+            this.Controls.Add(this.CourseNameTextBox);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.ViewButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.CoursesGridView);
+            this.Name = "Form1";
+            this.Text = "Courses CRUD App";
+            ((System.ComponentModel.ISupportInitialize)(this.CoursesGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ StudentCourse.WinForm/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentCourse.WinForm
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=db_student_course;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Courses (CourseID, CourseName) VALUES (@CourseID, @CourseName)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseID", CourseIDTextBox.Text);
+                    cmd.Parameters.AddWithValue("@CourseName", CourseNameTextBox.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Course added successfully.");
+                    ClearFields();
+                }
+            }
+        }
+
+        private void ViewButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Courses";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    CoursesGridView.DataSource = dt;
+                }
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Courses SET CourseName = @CourseName WHERE CourseID = @CourseID";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseID", CourseIDTextBox.Text);
+                    cmd.Parameters.AddWithValue("@CourseName", CourseNameTextBox.Text);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Course updated successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Course not found.");
+                    }
+                    ClearFields();
+                }
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM Courses WHERE CourseID = @CourseID";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseID", CourseIDTextBox.Text);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Course deleted successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Course not found.");
+                    }
+                    ClearFields();
+                }
+            }
+        }
+
+        private void ClearFields()
+        {
+            CourseIDTextBox.Text = "";
+            CourseNameTextBox.Text = "";
+        }
+    }
+}
